80f8c98f98bc64c978aad49e4722cc74bf92fbb6
Melania P Chagas <melaniapchagas@gmail.com>
2023-03-28 12:16:48 -0300

    feat: implementando função 'scrape_next_page_link'

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -28,7 +28,12 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page = selector.css('a.next.page-numbers ::attr(href)').get()
+    if next_page:
+        return next_page
+    else:
+        return None
 
 
 # Requisito 4
@@ -39,3 +44,9 @@ def scrape_news(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+if __name__ == '__main__':
+    html = fetch('https://blog.betrybe.com')
+    next_page = scrape_next_page_link(html)
+    print(next_page)
